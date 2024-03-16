edc19a5947612fb6e0e5e52486cedd356724be6f
Emil Kristina <120439047+kristina-emil@users.noreply.github.com>
2024-03-16 21:41:50 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,32 +8,9 @@ app = FastAPI()
 
 @app.get("/",response_class=HTMLResponse)
 def read_root():
-    return """
-    <html>
-    <head>
-        <title>Привет</title>
-        <style>
-            body {
-                background-color: DeepPink;
-                color: yellow;
-                font-family: Arial, sans-serif;
-            }
-            h1 {
-                text-align: center;
-                font-size: 36px;
-            }
-            img {
-                display: block;
-                margin: 0 auto 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <h1 style="text-align:center; font-size:99px;">Привет</h1>
-        <img src="/static/hello.jpg" alt="Hello Image" style="display:block; margin:auto;width:200px;">
-    </body>
-    </html>
-    """
+    with open("index.html", "r") as file:
+        return file.read()
+    
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
